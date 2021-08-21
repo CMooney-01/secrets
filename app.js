5f0f55179cbb47ab9ccdaaba5c778578a061bc49
CMooney-01 <chris.mooney@blackflagbarbell.com.au>
2021-08-21 10:30:23 +1000

    Update password security from cipher and .env to hash protection using bcrypt

--- app.js
+++ app.js
@@ -2,14 +2,16 @@
 require('dotenv').config();
 const express = require('express');
 const ejs = require('ejs');
-const {MongoClient} = require('mongodb');
-const uri = 'mongodb://localhost:27017';
-
 const crypto = require('crypto');
 
-const app = express();
+const {MongoClient} = require('mongodb');
+const uri = 'mongodb://localhost:27017';
 const client = new MongoClient(uri);
 
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+const app = express();
 app.use(express.static('public'));
 app.use(express.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
@@ -30,18 +32,18 @@ app.post('/login', function(req, res) {
     try {
       await client.connect();
       const foundUser = await client.db('userDB').collection('users').findOne({username: user});
-      let encrypted = foundUser.password;
+      let hash = foundUser.password;
 
       if (foundUser == undefined) {
         res.render('home');
       } else {
-        let decipher = crypto.createDecipheriv('aes-256-cbc', process.env.KEY, process.env.IV);
-        let decrypted = decipher.update(encrypted, 'hex', 'utf-8');
-        decrypted += decipher.final('utf-8');
-
-        if (decrypted === password) {
-          res.render('secrets');
-        }
+        bcrypt.compare(password, hash, function(err, result) {
+          if (result === true) {
+            res.render('secrets');
+          } else if (result === false) {
+            res.render('home');
+          }
+        });
       }
     } catch (err) {
       console.log(err);
@@ -59,33 +61,22 @@ app.get("/register", function(req, res) {
 
 app.post("/register", function(req, res) {
 
-  let password = req.body.password;
-
-  let cipher = crypto.createCipheriv('aes-256-cbc', process.env.KEY, process.env.IV);
-  let encrypted = cipher.update(password, 'utf-8', 'hex');
-  encrypted += cipher.final('hex');
-
-  async function createUser() {
-    try {
-      await client.connect();
-      const addUser = await client.db('userDB').collection('users').insertOne({username: req.body.username, password: encrypted});
-      console.log('User registered in database.');
-    } catch (err) {
-      console.log(err);
-    } finally {
-      await client.close();
+  let password = bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    async function createUser() {
+      try {
+        await client.connect();
+        const addUser = await client.db('userDB').collection('users').insertOne({username: req.body.username, password: hash});
+        console.log('User registered in database.');
+      } catch (err) {
+        console.log(err);
+      } finally {
+        await client.close();
+      }
     }
-  }
-
-  createUser();
-  res.render("secrets");
+    createUser();
+    res.render("secrets");
+  });
 });
-
-
-
-
-
-
 
 app.listen(3000, function() {
   console.log('Server started on port 3000.');
